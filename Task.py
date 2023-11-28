17039c5095770b2005086b0b65b0549803bbec27
Connerkirkley <141285547+Connerkirkley@users.noreply.github.com>
2023-11-27 18:23:06 -0700

    Add files via upload

---
+++ Task.py
@@ -0,0 +1,56 @@
+print('Hello, this is an interactable task editor that resembles a piece of paper. Please note that its not all powerful and will help with everyday tasks.')
+
+class Task_editor():
+    Task_List1 = [{'ID':1,'Task name':'Brush teeth','Status':'uncompleted','key word':'brush'},
+                       {'ID':2,'Task name':'Shower','Status':'Uncompleted','Key word':'Showar'},
+                       {'ID':3,'Task name':'Eat breakfast','Status':'uncompleted','Key word':'PPMD'}]
+    def __init__(self, ID, Task_name, Status, key_word):
+        self.ID = ID
+        self.Task_name = Task_name
+        self.Status = Status
+        self.key_word = key_word
+
+
+    def ABC_value():
+    
+
+
+    def ID_Value():
+
+
+
+    def Altering_Task_List():
+
+
+
+    def Add_Tasks():
+        if Input_Task == 'ADDTASK':
+
+
+
+    def Remove_Tasks():
+
+
+
+    def Mark_Up_task():
+
+
+
+    def Search_Task():
+    
+
+
+    def Undo_Action():
+
+
+
+class Display(Task_editor):
+
+    def Screen_And_input():
+        
+
+
+
+
+
+{'ID':,'Task name':'','Status':'','Key word':''}
