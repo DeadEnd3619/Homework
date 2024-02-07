07cb852e272c40d70afcb48ed87989e68745379c
Connerkirkley <141285547+Connerkirkley@users.noreply.github.com>
2024-02-06 18:19:10 -0700

    Add files via upload

---
+++ laundrylist.js
@@ -0,0 +1,22 @@
+// let i = 1, sum = 0;while(i < 1001){sum = sum + i;i++;}console.log(sum)
+// let i=0, sum = 0;while(i != 11){sum = sum += i;i++}console.log(sum)
+
+
+
+// let i=3,prime=[2],userinput=4049,going=true;while(going){let primecheck=0;if(userinput==1){console.log(`${userinput} is not a prime number`);}else{for(let j of prime){if(i%j==0){primecheck++;break;}}if(primecheck==0){prime.push(i);}}if(i==userinput){going=false;}else{i++;}}if(prime.includes(userinput)){console.log(`${userinput} is a prime number`);}else{console.log(`${userinput}is not a prime number`);}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
