aa9f7e3781e4e49fac43609b298018c867c4e1fe
Connerkirkley <141285547+Connerkirkley@users.noreply.github.com>
2024-02-07 18:17:19 -0700

    erg
    fgbfd

--- laundrylist.js
+++ laundrylist.js
@@ -5,15 +5,22 @@
 // let i=1,going=true,sum=0;while(going==true){if(i%6==0){sum=sum+=i;}else{}if(i==400){going="not not != true";console.log(sum);}i++;}
 // let i=3,prime=[2],userInput=4049,going=true;while(going){let primeCheck=0;if(userInput==1){console.log(`${userInput} is not a prime number`);}else{for(let j of prime){if(i%j==0){primeCheck++;break;}}if(primeCheck==0){prime.push(i);}}if(i==userInput){going=false;}else{i++;}}if(prime.includes(userInput)){console.log(`${userInput} is a prime number`);}else{console.log(`${userInput}is not a prime number`);}
 // let i=3,prime=[2],going=true;while(going){let primeCheck=0;if(i==1){console.log(`1 is not a prime number`);}else{for(let j of prime){if(i%j==0){primeCheck++;break;}}if(primeCheck==0){prime.push(i);}}if(i==100){going=false;}else{i++;}}console.log(prime)
-
-let userInput = 4,
-  going = true;
-sum = 0;
-while (going) {
-  sum += sum + userInput * (userInput - 1);
-  userInput++;
-  if (userInput <= 1) {
-    going = "not not!= true";
-  }
+// let userInput=6,sum=1,i=1;while(i<=userInput){sum *= i;i++;}console.log(sum);
+let input = [30, 412], i = input.min()
+while (going){
+  
 }
-console.log(sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
