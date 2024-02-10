bb9ac88c691f8cd3704803b851052c1b5cf3f038
Connerkirkley <141285547+Connerkirkley@users.noreply.github.com>
2024-02-09 18:07:42 -0700

    hdhbv

---
+++ coding/AddWalkthrough/AddWalkThrough.js
@@ -0,0 +1,20 @@
+
+const add = ()=>{
+    var num1 = parseFloat(document.getElementById('num1').value)
+    var num2 = parseFloat(document.getElementById('num2').value)
+    var result = num1 + num2
+    document.getElementById('result').innterHTML = "Result: "+ result
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
