567f4783964520314ec54eb3316e0bf7eb9f7c49
Connerkirkley <ckirkl439@west-mec.org>
2024-02-26 12:45:09 -0800

    Reduced complexity
    Changed the playBoard from a 2D array to a single array. Changed logic for former change.

--- ticTacToe/main.js
+++ ticTacToe/main.js
@@ -1,31 +1,38 @@
-var playBoard = [[null,null,null], [null,null,null], [null,null,null]];
+var playBoard = [null,null,null,null,null,null,null,null,null];
 var playerTurn = 'Player one'
 var turnXO = 'X'
-var winningpos = [[[0,0],[0,1],[0,2]], [[1,0],[1,1],[1,2]], [[2,0],[2,1],[2,2]], [[0,0],[1,0],[2,0]], [[0,1],[1,1],[2,1]], [[0,2],[1,2],[2,2]], [0,0],[1,1],[2,2], [[0,2],[1,1],[2,0]]]
-function getBoardPos(playBoardpos){
-    var index1 = (playBoardpos % 3)
+var winningPos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+function getBoardPos(playBoardPos){
+    var index = playBoardPos - 1
     if (playerTurn == 'Player one'){
         turnXO = 'X'
     }else{
         turnXO = 'O'
     }
-    if (playBoardpos == 1 || playBoardpos == 2 || playBoardpos == 3){
-        doPlayerAction(index1, 0)
-    }else if (playBoardpos == 4 || playBoardpos == 5 || playBoardpos == 6){
-        doPlayerAction(index1, 1)
+    if (playBoardPos == 1 || playBoardPos == 2 || playBoardPos == 3){
+        doPlayerAction(index)
+    }else if (playBoardPos == 4 || playBoardPos == 5 || playBoardPos == 6){
+        doPlayerAction(index)
     }else{
-        doPlayerAction(index1, 2)
+        doPlayerAction(index)
     }
 }
-function doPlayerAction(index2, index1){
-    if (playBoard[index1][index2] == null){
-        playBoard[index1][index2] = turnXO
+function doPlayerAction(index){
+    if (playBoard[index] == null){
+        playBoard[index] = turnXO
         
         if (playerTurn == 'Player one'){
             playerTurn = 'Player two'
         }else{
             playerTurn = 'Player one'
         }
+    }
+    else{
+        console.log('invalid input')
+    }
+    let boolBoard = [bool(playBoard[0]), bool(playBoard[1]), bool(playBoard[2]), bool(playBoard[3]), bool(playBoard[4]), bool(playBoard[5]), bool(playBoard[6]), bool(playBoard[7]), bool(playBoard[8])]
+    for (let i of winningPos){
+        
     }
     console.log(playBoard)
 }
