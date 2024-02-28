243c0738834e9e83a8d9c79d67d503faffccf860
Connerkirkley <141285547+Connerkirkley@users.noreply.github.com>
2024-02-27 18:23:26 -0700

    Added files.
    Added test files and made progress on TicTacToe Project.

---
+++ JavaScriptStuff/wat/wat.js
@@ -0,0 +1,18 @@
+let stuff = document.createElement('p')
+stuff.className = 'Bernard'
+stuff.innerHTML = 'Conner Kirkley'
+document.body.append(stuff)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ticTacToe/main.js
+++ ticTacToe/main.js
@@ -1,4 +1,5 @@
-var playBoard = [0,0,0,0,0,0,0,0,0],playerTurn = 'Player one',turnXO = 'X',winningPos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]],turns = 0,game = true, formerTurn = 'X'
+var playBoard = [0,0,0,0,0,0,0,0,0],playerTurn = 'Player one',turnXO = 'X',winningPos = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]],turns = 0,game = true, formerTurn = 'X', winningPlayer = ''
+document.getElementById('player').innerHTML = playerTurn
 function getBoardPos(playBoardPos){
     var index = playBoardPos - 1
     if (playerTurn == 'Player one'){
@@ -14,12 +15,8 @@ function getBoardPos(playBoardPos){
 function doPlayerAction(index){
     if (playBoard[index] == 0){
         playBoard[index] = turnXO
-        doHTMLEdit(index)
-        turns++
-        if (playerTurn == 'Player one'){
-            playerTurn = 'Player two'
-        }else{
-            playerTurn = 'Player one'}}
+        doHTMLEdit(index, 1)
+        turns++}
     else{
         console.log('invalid input')}
     let boolBoard = [Boolean(playBoard[0]), Boolean(playBoard[1]), Boolean(playBoard[2]), Boolean(playBoard[3]), Boolean(playBoard[4]), Boolean(playBoard[5]), Boolean(playBoard[6]), Boolean(playBoard[7]), Boolean(playBoard[8])]
@@ -28,12 +25,28 @@ function doPlayerAction(index){
         if (boolBoard[i1] && boolBoard[i2] && boolBoard[i3]){
             if (playBoard[i1] == playBoard[i2] && playBoard[i2] == playBoard[i3]){
                 console.log('winner');
-                game = false;}}}
+                game = false;
+                winningPlayer = playBoard[i1]
+                doHTMLEdit(index, 2)
+            }}}
     if (turns == 9 && game == true){
         console.log('Tie')}}
-function doHTMLEdit(index){
+function doHTMLEdit(index, condition){
+    if (condition == 1){
     document.getElementById(index + 1).innerHTML = turnXO
-    console.log(playBoard)}
+    if (playerTurn == 'Player one'){
+        playerTurn = 'Player two'
+    }else{
+        playerTurn = 'Player one'}
+        document.getElementById('player').innerHTML = playerTurn}
+    if (condition == 2){
+        if (winningPlayer = 'X'){
+            winningPlayer = 'Player one'
+        }else{
+            winningPlayer = 'Player two'
+        }
+        document.getElementById('player').innerHTML = `${winningPlayer} has won`
+    }}
 
 
 
